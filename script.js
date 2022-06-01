39aec250225cd04ca51ebca382b3b45f3b5ff8b0
wise-guru <wise.guru123@gmail.com>
2022-06-01 18:39:29 -0400

    Add color option buttons

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
 const grid = document.querySelector('#grid');
-const newGrid = document.querySelector('#newGrid');
+let cells = grid.querySelectorAll('div');
+let color = 'black';
 
 
 
 function createGrid(input) {
     grid.style.gridTemplateColumns = `repeat(${input}, 1fr)`;
-grid.style.gridTemplateRows = `repeat(${input}, 1fr)`;
+    grid.style.gridTemplateRows = `repeat(${input}, 1fr)`;
+    
 
     for (let i = 0; i < Math.pow(input,2); i++) {
         cell = document.createElement('div');
         grid.appendChild(cell);
         cell.style.outline = "solid black 1px";
+        cell.style.backgroundColor = 'white';
 
-        cell.addEventListener('mouseenter', function(event) {
-            event.target.style.backgroundColor = ("background","rgb("+Math.floor(Math.random()*256)+","+Math.floor(Math.random()*256)+","+Math.floor(Math.random()*256)+")")
-        })
+        cell.addEventListener('mouseenter', sketchColor);
 
     }
 }
@@ -44,11 +45,6 @@ function reset() {
     cells.forEach((div) => div.style.backgroundColor = 'white');
   }
 
-/*
-function color() {
-    $(this).css("background","rgb("+Math.floor(Math.random()*256)+","+Math.floor(Math.random()*256)+","+Math.floor(Math.random()*256)+")")
-}
-*/
 
 const gridBtn = document.querySelector('#gridBtn');
 gridBtn.addEventListener('click', function() {
@@ -60,3 +56,42 @@ const resetBtn = document.querySelector('#resetBtn');
 resetBtn.addEventListener('click', function() {
     reset();
 })
+
+function defaultColor() {
+
+    this.style.backgroundColor = (color);
+
+    /*
+    
+    const rgb = document.querySelector('#rgb');
+    rgb.addEventListener('click', function() {
+            this.style.backgroundColor = ("background","rgb("+Math.floor(Math.random()*256)+","+Math.floor(Math.random()*256)+","+Math.floor(Math.random()*256)+")")
+        })
+    
+    const black = document.querySelector('#black');
+    black.addEventListener('click', function () {
+        this.style.backgroundColor = ('black');
+    })
+  
+    */
+
+}   
+
+
+/*
+cell.addEventListener('mouseenter', function(event) {
+            event.target.style.backgroundColor = ("background","rgb("+Math.floor(Math.random()*256)+","+Math.floor(Math.random()*256)+","+Math.floor(Math.random()*256)+")")
+*/
+
+function sketchColor() {
+    if (color === 'rgb') {
+        this.style.backgroundColor = ("background","rgb("+Math.floor(Math.random()*256)+","+Math.floor(Math.random()*256)+","+Math.floor(Math.random()*256)+")")
+    }
+    else {
+        this.style.backgroundColor = color;
+    }
+}
+
+function changeColor(choice) {
+    color = choice;
+}
