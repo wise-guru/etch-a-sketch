7956c7b4375b71473b51896496f00f7ba51509ac
wise-guru <wise.guru123@gmail.com>
2022-06-01 14:59:47 -0400

    add style

--- script.js
+++ script.js
@@ -2,63 +2,61 @@ const grid = document.querySelector('#grid');
 const newGrid = document.querySelector('#newGrid');
 
 
-grid.style.gridTemplateColumns = "repeat(16, 1fr)";
-grid.style.gridTemplateRows = "repeat(16, 1fr)";
 
-let cell = document.createElement('div');
-cell.classList.add('cell')
-grid.appendChild(cell);
+function createGrid(input) {
+    grid.style.gridTemplateColumns = `repeat(${input}, 1fr)`;
+grid.style.gridTemplateRows = `repeat(${input}, 1fr)`;
 
+    for (let i = 0; i < Math.pow(input,2); i++) {
+        cell = document.createElement('div');
+        grid.appendChild(cell);
+        cell.style.outline = "solid black 1px";
 
-for (let i = 0; i < 256; i++) {
-    cell = document.createElement('div');
-    grid.appendChild(cell);
-    cell.style.outline = "solid black 1px";
+        cell.addEventListener('mouseenter', function(event) {
+            event.target.style.backgroundColor = ("background","rgb("+Math.floor(Math.random()*256)+","+Math.floor(Math.random()*256)+","+Math.floor(Math.random()*256)+")")
+        })
 
-    cell.addEventListener('mouseenter', function(event) {
-        event.target.style.backgroundColor = 'red';
-    })
-
-}
-
-function reset() {
-    document
-      .querySelectorAll("#grid")
-      .forEach((e) => e.parentNode.removeChild(e));
-  }
-
-
-function userGridInput() {
-    let playerInput = parseInt(prompt("Enter the number of squares per each side for the new grid."));
-    reset();
-
-    
-    newGrid.style.gridTemplateColumns = `repeat(${playerInput}, 1fr)`;
-    newGrid.style.gridTemplateRows = `repeat(${playerInput}, 1fr)`;
-    newGrid.style.display = 'grid';
-    newGrid.style.width = '500px';
-    newGrid.style.height = '500px';
-    newGrid.style.outline = 'solid black 4px';
-    newGrid.style.margin = 'auto';
-
-
-  for (i = 0; i < Math.pow(playerInput, 2); i++) {
-
-    if (playerInput < 100) {
-        
-        let newCell = document.createElement('div');
-        newGrid.appendChild(newCell);
-        newCell.style.outline = "solid black 2px";
-    
-        newCell.addEventListener('mouseenter', function(event) {
-            event.target.style.backgroundColor = 'red';
-        })    
-        
     }
 }
+
+createGrid(16);
+
+function userGrid() {
+    input = parseInt(prompt("Enter the number of squares per each side for the new grid."));
+
+    if (input >= 2 || input < 100) {
+    createGrid(input);
+    }
+    else if (input < 2 || input > 100) {
+        console.log("Input is too low or too high")
+    }
+    else if (input != Number.isInteger()) {
+        console.log("Not a number.");
+    }
+
 }
+
+
+
+
+function reset() {
+    let cells = grid.querySelectorAll('div');
+    cells.forEach((div) => div.style.backgroundColor = 'white');
+  }
+
+/*
+function color() {
+    $(this).css("background","rgb("+Math.floor(Math.random()*256)+","+Math.floor(Math.random()*256)+","+Math.floor(Math.random()*256)+")")
+}
+*/
 
 const gridBtn = document.querySelector('#gridBtn');
 gridBtn.addEventListener('click', function() {
-    userGridInput();
+    reset();
+    userGrid();
+})
+
+const resetBtn = document.querySelector('#resetBtn');
+resetBtn.addEventListener('click', function() {
+    reset();
 })
